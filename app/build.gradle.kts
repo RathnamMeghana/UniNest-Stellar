923acd62c0101ce8cb3ee708677209be56932f1f
AoifeMurphy02 <aoifemurphy2019@gmail.com>
2025-11-13 16:44:31 +0000

    Added firebase initialization to the application with Richy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
+
+
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -37,7 +41,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.play.services.measurement.api)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
 }
