44fdacbe8174addeecb0f5830179ba0452d47384
AoifeMurphy02 <aoifemurphy2019@gmail.com>
2025-11-18 20:47:17 +0000

    ✨ ApartmentService.java
     ➕ added ApartmentService file with setup for creating an apartment and a get all apartment methods
      ➕ added ApartmentRequest.java file nothing in it yet

--- Backend/src/main/java/UniNest/Backend/dto/ApartmentRequests.java
+++ Backend/src/main/java/UniNest/Backend/dto/ApartmentRequests.java
@@ -1,3 +1,9 @@
+package UniNest.Backend.dto;
+
+import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+
+@Data
 public class ApartmentRequests {
-    
+
 }
---
+++ Backend/src/main/java/UniNest/Backend/service/ApartmentService.java
@@ -0,0 +1,57 @@
+package UniNest.Backend.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.cloud.FirestoreClient;
+
+import UniNest.Backend.dto.ApartmentRequests;
+import UniNest.Backend.model.Apartment;
+import UniNest.Backend.model.Building;
+
+@Service
+public class ApartmentService {
+    public String ApartmentService(ApartmentRequests request) {
+        Firestore db = FirestoreClient.getFirestore();
+
+        Apartment apartment = new Apartment();
+        apartment.setBuildingId(request.getBuildingId());
+        apartment.setName(request.getName());
+        apartment.setCode(request.getCode());
+        apartment.setLandlordId(request.getLandlordId());
+        apartment.setCreatedAt(request.getCreatedAt());
+        apartment.setActive(request.getActive());
+
+        db.collection("apartment").add(apartment);
+
+        return "Apartment created successfully";
+    };
+
+    public List<Apartment> getAllApartments() {
+        try {
+            Firestore db = FirestoreClient.getFirestore();
+
+            ApiFuture<QuerySnapshot> future = db.collection("apartments").get();
+            List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+
+            List<Apartment> apartments = new ArrayList<>();
+
+            for (QueryDocumentSnapshot doc : documents) {
+                Apartment apartment = doc.toObject(Apartment.class);
+                apartments.add(apartment);
+            }
+
+            return apartments;
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to fetch apartments: " + e.getMessage());
+        }
+    }
+
+}
