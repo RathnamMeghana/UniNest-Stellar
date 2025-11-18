a0bd9dd24b3c634c6d85e026ae25b2e7d1784eb3
AoifeMurphy02 <aoifemurphy2019@gmail.com>
2025-11-18 12:17:59 +0000

    ✨ Apartment
     ➕ Added Apartment data model
     ➕ Added file for Apartment controller
     ➕ Added file for Apartment requests

---
+++ Backend/src/main/java/UniNest/Backend/model/Apartment.java
@@ -0,0 +1,11 @@
+@Data
+public class Apartment {
+
+    private String buildingId;
+    private String name;
+    private String code;
+    private String landlordId;
+    private Timestamp createdAt;
+    private Boolean active;
+
+}
