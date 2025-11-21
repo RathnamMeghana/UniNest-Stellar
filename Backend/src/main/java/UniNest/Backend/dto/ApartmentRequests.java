083b12d64aa1246e1b77c9b509e3a02066b6b9f1
AoifeMurphy02 <aoifemurphy2019@gmail.com>
2025-11-21 19:12:44 +0000

    ✨ API integration with FrontEnd for Apartment
     ➕ added Apartment model to the app with getters and setters

--- Backend/src/main/java/UniNest/Backend/dto/ApartmentRequests.java
+++ Backend/src/main/java/UniNest/Backend/dto/ApartmentRequests.java
@@ -2,6 +2,8 @@ package UniNest.Backend.dto;
 
 
 
+import com.google.cloud.Timestamp;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import jakarta.validation.constraints.NotBlank;
@@ -11,10 +13,13 @@ public class ApartmentRequests {
     @NotBlank
     private String buildingId;
 
-
+    @NotBlank
     private String name;
-
+    private String code;
     private String landlordId;
+    private Timestamp createdAt;
+    private String description;
+    private Double rentPrice;
 
     @NotNull
     private Boolean active;
